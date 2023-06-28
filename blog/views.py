3e8b504fb28d77c0ca03753396adfe05d590bd6c
Kaue Camargo <kaue.mcamargo@gmail.com>
2023-06-28 09:58:26 -0300

    28/06

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+# Inclui a classe HttpResponse. 
+from django.http import HttpResponse
+
+# Define uma function view chamada index. 
+def index(request):
+    return HttpResponse('olá Django index')
+
+# Define uma function view chamada ola. 
+def ola(request):
+    return HttpResponse( 'olá Django')
+
