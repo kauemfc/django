376e0ca66e566f90baa249176f3593a3faecde2e
Kaue Camargo <kaue.mcamargo@gmail.com>
2023-09-13 10:02:11 -0300

    att hj

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import index, ola, post_show, PostDetailView
+from blog.views import index, ola, post_show, PostDetailView, get_all_posts, get_post
 
 urlpatterns = [
     path('', index, name= "home"),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('posts/all', ola, name="posts_list"),
     path('post/<int:post_id>', post_show, name="exibe_post"),
     path('post/<int:pk>/show', PostDetailView.as_view(), name="post_detail"),
+    path('api/posts', get_all_posts, name="posts_data"),
+    path('api/posts/<int:post_id>', get_post, name="post_data"),
 ]
