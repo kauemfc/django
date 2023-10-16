5687984b7bee4bfdfe9d81711bc58fce81e8d039
kauemfc <107476870+kauemfc@users.noreply.github.com>
2023-10-16 01:23:29 -0300

    Registro de usuários com novos campos

--- accounts/url.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from accounts import views
-urlpatterns = [
-  path('accounts/signup', # caminho que vai carregar a view com o formulário
-    views.AccountCreateView.as_view(),
-    name="signup"
-  ),
-]
